69180ae7b130b9591919bf1d42b3be1a0fb9888f
dscyrescotti <dscyrescotti@gmail.com>
2021-04-11 22:25:22 +0630

    feat: finish getting code

--- Spotify/Resources/SceneDelegate.swift
+++ Spotify/Resources/SceneDelegate.swift
@@ -15,7 +15,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ViewController()
+        if AuthManager.shared.isSignedIn {
+            window.rootViewController = TabBarViewController()
+        } else {
+            window.rootViewController = UINavigationController(rootViewController: WelcomeViewController())
+        }
         self.window = window
         window.makeKeyAndVisible()
     }
