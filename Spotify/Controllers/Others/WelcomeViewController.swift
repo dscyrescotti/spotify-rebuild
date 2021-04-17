735fd1d6984f180fe5a8f3a4de22e2d827dde54f
dscyrescotti <dscyrescotti@gmail.com>
2021-04-17 19:21:07 +0630

    feat: finish app

--- Spotify/Controllers/Others/WelcomeViewController.swift
+++ Spotify/Controllers/Others/WelcomeViewController.swift
@@ -16,6 +16,36 @@ class WelcomeViewController: UIViewController {
         button.setTitleColor(.black, for: .normal)
         return button
     }()
+    
+    private let backgroundView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "albums-background")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.opacity = 0.65
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let logoView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "logo")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.text = "Listen to Millions of Songs on the go"
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 26, weight: .bold)
+        label.textColor = .white
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,7 +78,12 @@ extension WelcomeViewController {
     func setUp() {
         title = "Spotify"
         navigationController?.navigationBar.prefersLargeTitles = true
-        view.backgroundColor = .systemGreen
+        self.navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
+        
+        view.backgroundColor = .black
+        view.addSubview(backgroundView)
+        view.addSubview(logoView)
+        view.addSubview(label)
         view.addSubview(signInButton)
         signInButton.addTarget(self, action: #selector(tapped), for: .touchUpInside)
     }
@@ -56,5 +91,21 @@ extension WelcomeViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         signInButton.frame = CGRect(x: 20, y: view.bottom - 50 - view.safeAreaInsets.bottom, width: view.width - 40, height: 50)
+        NSLayoutConstraint.activate([
+            backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            logoView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -40),
+            logoView.widthAnchor.constraint(equalToConstant: 150),
+            logoView.heightAnchor.constraint(equalToConstant: 150),
+            
+            label.topAnchor.constraint(equalTo: logoView.bottomAnchor, constant: 10),
+            label.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor, constant: 90),
+            label.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: -90),
+
+        ])
     }
 }
